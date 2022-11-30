e86e78d1e03cdb6ad05a2a1e8ca1fc08583890ad
ershan <ershan_coding@outlook.com>
2022-12-01 00:34:13 +0800

    [logseq-plugin-git:commit] 2022-11-30T16:34:13.890Z

---
+++ assets/hello_world.py
@@ -0,0 +1,11 @@
+# hello_world.py ①
+from rich import print  # ②
+
+
+def main():  # ③
+  hello_world = '[red]hello[/red] [blue]world[/blue]'  # ④
+  print(hello_world)  # ⑤
+  
+
+if __name__ == '__main__':  # ⑥
+  main()  # ⑦
